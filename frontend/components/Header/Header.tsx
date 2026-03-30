227b477dcaa330a422627f7cf98f93b524f20350
Oleg Shapoval <olegkazhan@gmail.com>
2026-03-31 00:23:59 +0300

    bottom border for active: Shop & Cart

--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -3,89 +3,37 @@ import css from './Header.module.css'
 import Image from 'next/image'
 import icon from '../../public/logo-delivery.png'
 import {
-  HiOutlineMenu,
-  HiOutlineX,
   HiOutlineShoppingCart,
 } from 'react-icons/hi'
 import React from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function Header() {
-  const [isOpen, setIsOpen] = React.useState(false)
-
-  const toggleMenu = () => {
-    setIsOpen(prev => !prev)
-    document.body.style.overflow = !isOpen ? 'hidden' : 'unset'
-  }
+  const pathname = usePathname()
   return (
     <header className={css.header}>
       <div className={css.headerContainer}>
-        <button
-          onClick={toggleMenu}
-          className={css.burgerBtn}
-          aria-label="toggle-menu"
-        >
-          <div className={css.iconContainer}>
-            <HiOutlineX
-              size={30}
-              className={`${css.icon} ${isOpen ? css.visible : css.hidden}`}
-            />
-            <HiOutlineMenu
-              size={30}
-              className={`${css.icon} ${isOpen ? css.hidden : css.visible}`}
-            />
-          </div>
-        </button>
+        <ul className={css.headerTextContainer}>
+          <Link href="/">
+            <li className={pathname === '/' ? css.active : ''}>Shop</li>
+          </Link>
+          <Link href="/cart">
+            <li className={pathname === '/cart' ? css.active : ''}>
+              Shopping Cart
+            </li>
+          </Link>
+        </ul>
+
+        <Link href="/cart" className={css.cart}>
+          <HiOutlineShoppingCart size={25} color="#df2727" />
+        </Link>
         <div className={css.logoDiv}>
           <Link href="/">
             <Image src={icon} height={40} alt="logo" priority />
           </Link>
         </div>
       </div>
-      <AnimatePresence>
-        {isOpen && (
-          <>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              onClick={toggleMenu}
-              className={css.backdrop}
-            />
-
-            <motion.nav
-              initial={{ x: '-100%' }}
-              animate={{ x: 0 }}
-              exit={{ x: '-100%' }}
-              transition={{ type: 'tween' }}
-              className={css.menu}
-            >
-              <ul className={css.mobileMenuTextContainer}>
-                <li>
-                  <Link
-                    href="/"
-                    className={css.mobileMenuText}
-                    onClick={toggleMenu}
-                  >
-                    Shop
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/cart"
-                    className={css.mobileMenuText}
-                    onClick={toggleMenu}
-                  >
-                    Shopping Cart
-                    <HiOutlineShoppingCart />
-                  </Link>
-                </li>
-              </ul>
-            </motion.nav>
-          </>
-        )}
-      </AnimatePresence>
     </header>
   )
 }
