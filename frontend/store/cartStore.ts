1df7760a370d14d852aef36c1a9753bcded64247
Oleg Shapoval <olegkazhan@gmail.com>
2026-03-31 10:13:35 +0300

    zustand cartstore

---
+++ frontend/store/cartStore.ts
@@ -0,0 +1,37 @@
+import { create } from 'zustand'
+
+type CartItem = {
+  productId: string,
+  name: string,
+  price: number,
+  quantity: number,
+}
+
+type CartStore = {
+  items: CartItem[],
+  addToCart: (item: CartItem) => void,
+  removeFromCart: (productId: string, quantity: number) => void,
+  updateQuantity: (productId: string, quantity: number) => void,
+  clearCart: () => void
+}
+
+export const useCartStore = create<CartStore>((set) => ({
+  items: [],
+  addToCart: (item) => set((state) => {
+    const existing = state.items.find(i => i.productId === item.productId)
+    if (existing) return {
+      items: state.items.map(i => i.productId === item.productId ? {
+        ...i,
+        quantity: i.quantity + 1,
+      } : i),
+    }
+    return { items: [...state.items, item] }
+  }),
+  removeFromCart: (productId) => set((state) => (
+    { items: state.items.filter(i => i.productId !== productId) }
+  )),
+  updateQuantity: (productId, quantity) => set(state => ({
+    items: state.items.map(i => i.productId === productId ? { ...i, quantity } : i),
+  })),
+  clearCart: () => set({ items: [] }),
+}))
