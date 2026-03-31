6ab92b6e3590e427696c073e1b68fb8ed38ebbfc
Oleg Shapoval <olegkazhan@gmail.com>
2026-03-31 14:56:52 +0300

    origin: "*"

--- backend/server.ts
+++ backend/server.ts
@@ -14,7 +14,7 @@ connectDB().catch((err) => {
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
