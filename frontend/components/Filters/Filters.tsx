9d518b9eae5eeadf9553fb3eb326eb63f70ae73a
Oleg Shapoval <olegkazhan@gmail.com>
2026-04-01 10:38:31 +0300

    del unused component - now active shop styled on all devices

--- frontend/components/Filters/Filters.tsx
+++ frontend/components/Filters/Filters.tsx
@@ -1,14 +1,22 @@
+'use client'
 import css from './Filters.module.css'
-import Link from 'next/link'
 import { getShops } from '../services/getShops'
-import ShopsFilterDesktopClient from './ShopsFilterDesktop.client'
+import { useEffect, useState } from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 type FiltersProps = {
   variant: 'desktop' | 'mobile'
 }
 
-export default async function Filters({ variant }: FiltersProps) {
-  const shops = await getShops()
+export default function Filters({ variant }: FiltersProps) {
+  const [shops, setShops] = useState<string[]>([])
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const storeName = searchParams.get('storeName')
+  const router = useRouter()
+  useEffect(() => {
+    getShops().then(data => setShops(data))
+  }, [])
 
   if (variant === 'mobile') {
     return (
@@ -16,18 +24,40 @@ export default async function Filters({ variant }: FiltersProps) {
         <p className={css.mobileTitle}>Shops:</p>
         <ul className={css.mobileShopsList}>
           {shops.map(shop => (
-            <li key={shop}>
-              <Link
-                className={css.mobileShopLink}
-                href={{ pathname: '/', query: { storeName: shop } }}
-              >
-                {shop}
-              </Link>
+            <li
+              key={shop}
+              className={`${css.mobileShopLink} ${
+                pathname === '/' && storeName === shop ? css.active : ''
+              }`}
+              onClick={() => {
+                const params = new URLSearchParams({ storeName: shop })
+                router.push(`/?${params.toString()}`)
+              }}
+            >
+              {shop}
             </li>
           ))}
         </ul>
       </div>
     )
   }
-  return <ShopsFilterDesktopClient />
+  return (
+    <div className={css.sideShops}>
+      <p className={css.titleShops}>Shops:</p>
+      <ul className={css.shopsList}>
+        {shops.map(shop => (
+          <li
+            key={shop}
+            className={pathname === '/' && storeName === shop ? css.active : ''}
+            onClick={() => {
+              const params = new URLSearchParams({ storeName: shop })
+              router.push(`/?${params.toString()}`)
+            }}
+          >
+            {shop}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
