c91371751282359567be966f1460f45e3e245767
Oleg Shapoval <olegkazhan@gmail.com>
2026-04-04 07:18:34 +0300

    desktop categories filters shows and hide by click

--- frontend/components/Filters/Filters.tsx
+++ frontend/components/Filters/Filters.tsx
@@ -1,7 +1,10 @@
+'use client'
 import css from './Filters.module.css'
 import Link from 'next/link'
 import { FiltersProps } from '../../types/types'
 import { buildHref } from '../../utils/buildHref'
+import { FiChevronDown } from 'react-icons/fi'
+import { useState } from 'react'
 
 export default function Filters({
                                   shops = [],
@@ -9,9 +12,11 @@ export default function Filters({
                                   category,
                                   categories = [],
                                 }: FiltersProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
   return (
     <div className={css.sideShops}>
-      <ul className={css.shopsList}>
+      <ul className={css.categoriesInner}>
         <p className={css.titleShops}>Shops:</p>
         {shops.map(shop => (
           <li key={shop._id}>
@@ -23,18 +28,29 @@ export default function Filters({
             </Link>
           </li>
         ))}
-        <p className={css.titleShops}>Categories:</p>
-        {categories.map(cat => (
-          <li key={cat}>
-            <Link
-              href={buildHref({ storeName, category: cat })}
-              className={cat === category ? css.active : ''}
-            >
-              {cat}
-            </Link>
-          </li>
-        ))}
       </ul>
+      <div className={css.titlePlusIcon}>
+        <p className={css.titleShops}>Categories</p>
+        <FiChevronDown
+          onClick={() => setIsOpen(prev => !prev)}
+          size={20}
+          className={`${css.chevron} ${isOpen ? css.chevronOpen : ''}`}
+        />
+      </div>
+      <div className={`${css.categoriesWrapper} ${isOpen ? css.open : ''}`}>
+        <ul className={css.categoriesInner}>
+          {categories.map(cat => (
+            <li key={cat}>
+              <Link
+                href={buildHref({ storeName, category: cat })}
+                className={cat === category ? css.active : ''}
+              >
+                {cat}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
