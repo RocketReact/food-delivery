0822a51a7fa031263a803dd4c2a7dd393405e76d
Oleg Shapoval <olegkazhan@gmail.com>
2026-04-06 17:02:23 +0300

    getOrder.ts - get order from backend

---
+++ frontend/services/getOrders.ts
@@ -0,0 +1,34 @@
+import { CartItem, SearchOrdersProps } from '../types/types'
+import { OrderForm } from '../app/cart/page'
+
+export type OrdersResProps = {
+  _id: string
+  customer: OrderForm
+  items: CartItem[]
+  totalPrice: number
+  created_at: string
+}
+
+export async function getOrders(
+  orderData?: SearchOrdersProps,
+): Promise<OrdersResProps[]> {
+  const params = new URLSearchParams()
+
+  if (orderData?._id) params.set('_id', orderData._id)
+  if (orderData?.email) params.set('email', orderData.email)
+  if (orderData?.phone) params.set('phone', orderData.phone)
+
+  const query = params.toString()
+  const url = `${process.env.NEXT_PUBLIC_URL_SERVER}/orders${query ? `?${query}` : ''}`
+  try {
+    const res = await fetch(url, {
+      cache: 'no-cache',
+    })
+    if (res.ok) return res.json()
+    console.error('Failed to fetch orders', res.status)
+    return []
+  } catch (error) {
+    console.error('Failed to fetch orders', error)
+    return []
+  }
+}
